53fcb156f44ca5b231df8e42ba86195d57e6a733
Anders Lundstedt <git@anderslundstedt.se>
2025-09-25 16:04:57 +0200

    some more tests

--- parsing-operators/test.m
+++ parsing-operators/test.m
@@ -41,31 +41,6 @@ r_a --> ['a'].
 r_b --> ['b'].
 
 
-%% DCG RULES R_CHR_EXCEPT_FOR
-
-:- pred r_chr_except_for(ta_chr,               ta_chr, ta_chrs, ta_chrs).
-:- mode r_chr_except_for(in,                   out,    in,      out) is semidet.
-
-:- pred r_chr_except_for(ta_chr,ta_chr,        ta_chr, ta_chrs, ta_chrs).
-:- mode r_chr_except_for(in,    in,            out,    in,      out) is semidet.
-
-:- pred r_chr_except_for(ta_chr,ta_chr,ta_chr, ta_chr, ta_chrs, ta_chrs).
-:- mode r_chr_except_for(in,    in,    in,     out,    in,      out) is semidet.
-
-r_chr_except_for(        C1,    C2,    C3,     C_OUT) --> (
-  [C_OUT],
-  {not C_OUT = C1, not C_OUT = C2, not C_OUT = C3}
-).
-r_chr_except_for(        C1,    C2,            C_OUT) --> (
-  [C_OUT],
-  {not C_OUT = C1, not C_OUT = C2}
-).
-r_chr_except_for(        C1,                   C_OUT) --> (
-  [C_OUT],
-  {not C_OUT = C1}
-).
-
-
 %% HELPER FUNCTION STR2CHRS
 
 :- func str2chrs(ta_str) = ta_chrs.
@@ -77,16 +52,35 @@ str2chrs(S)              = string.to_char_list(S).
 %%% TESTS 1A AND TEST 1B
 
 :- pred r_1(ta_chrs::in,ta_chrs::out) is semidet.
-r_1 --> +([
-  r_a,
-  +([r_b])
-]).
+r_1 --> +([r_a, +([r_b])]).
 
 :- pred test_1a is semidet.
 test_1a :- r_1(str2chrs("ababbbabc"),['c']).
 
 :- pred test_1b is semidet.
 test_1b :- not r_1(str2chrs("ababa"),str2chrs("ababa")).
+
+%%% TESTS 2A AND TEST 2B
+
+:- pred r_2(ta_chrs::in,ta_chrs::out) is semidet.
+r_2 --> +([r_a, ?([r_b])]).
+
+:- pred test_2a is semidet.
+test_2a :- r_2(str2chrs("abaaaaaabc"),['c']).
+
+:- pred test_2b is semidet.
+test_2b :- not r_2(str2chrs("aba"),str2chrs("aba")).
+
+%%% TESTS 3A AND TEST 3B
+
+:- pred r_3(ta_chrs::in,ta_chrs::out) is semidet.
+r_3 --> ?([r_a, *([r_b])]).
+
+:- pred test_3a is semidet.
+test_3a :- r_3(str2chrs("abbbbbbbc"),['c']).
+
+:- pred test_3b is semidet.
+test_3b :- r_3(str2chrs("cab"),str2chrs("cab")).
 
 %%% THE PREDICATE
 
@@ -101,6 +95,30 @@ test(!IO) :- (
   (
     if not test_1b then
       io.write_string("test 1b failed\n",!IO)
+    else
+      true
+  ),
+  (
+    if not test_2a then
+      io.write_string("test 2a failed\n",!IO)
+    else
+      true
+  ),
+  (
+    if not test_2b then
+      io.write_string("test 2b failed\n",!IO)
+    else
+      true
+  ),
+  (
+    if not test_3a then
+      io.write_string("test 3a failed\n",!IO)
+    else
+      true
+  ),
+  (
+    if not test_3b then
+      io.write_string("test 3b failed\n",!IO)
     else
       true
   )
